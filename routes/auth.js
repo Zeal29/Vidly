8db16c56a76872a1eeada85f1bab0c43748eb013
Ali Nawaz <ali209092@gmail.com>
2022-02-09 13:51:46 +0500

    commit

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,6 @@ const bcrypt = require( "bcrypt" )
 const express = require( 'express' );
 const router = express.Router();
 const Joi = require( 'joi' );
-const jsonwebtoken = require( "jsonwebtoken" );
 
 
 router.post( '/', async ( req, res ) =>
